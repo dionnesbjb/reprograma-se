4b191b80e80f2fa023a2ad324155d3efe42922c2
dionnesbjb <dionnesbjb@hotmail.com>
2022-12-21 15:28:10 -0300

    reprograma-se

---
+++ FIC-01/Módulo03/Semana03/Solução/Semana03/F1-M3-Sem03-Praticas.js
@@ -0,0 +1,17 @@
+var pares = 0;
+var impares = 0;
+
+
+for (let i = 1; i <= 20; i++) {   // 2 pontos
+  var quadrado = i * i;
+  console.log(quadrado);
+  if (quadrado % 2 == 0) {        // 2 pontos
+    pares = pares + quadrado;     // 2 pontos
+  } else {                        // 2 pontos  
+    impares = impares + quadrado; // 2 pontos
+  }
+}
+
+console.log("A soma de quadrados pares: ", pares);
+console.log("A soma de quadrados ímpares: ", impares);
+

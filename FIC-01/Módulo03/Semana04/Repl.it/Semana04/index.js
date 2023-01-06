37fef9aa00a6cc0e52b16ca05e298abc8652ec1d
dionnesbjb <dionnesbjb@hotmail.com>
2023-01-06 16:10:12 -0300

    reprograme-se

---
+++ FIC-01/Módulo03/Semana04/Repl.it/Semana04/index.js
@@ -0,0 +1,10 @@
+const prompt = require('prompt-sync')();
+
+alturas = []
+
+// Implementar a entrada de dados
+
+// Implementar o processamento de dados
+// Fazer for dentro de for
+
+// Implementar a saída de dados

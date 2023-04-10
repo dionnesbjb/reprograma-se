de51cbae694f53a2c7828d750b1ca3067fd6dba1
dionnesbjb <dionnesbjb@hotmail.com>
2023-04-10 16:29:02 -0300

    reprograma-se

--- FIC-02/Módulo02/Aula08/index.js
+++ FIC-02/Módulo02/Aula08/index.js
@@ -1,21 +1,21 @@
 const http = require('http')
 const porta = 443
 
-const servidor = http.createServer((req, res)=> {
-res.writeHead(200,{'Content-Type': 'text/html'})
-if(req.url == '/'){
-res.write('<h1>Seja bem Vindo! </h1')
-}
-else if(req.url == '/server'){
-res.write('<h1>Server </h1')
-}
-else if(req.url == '/server/node'){
-res.write('<h1>Server Web no Node </h1')
-}
-else {
-res.write('<h1>PADRAO </h1')
-}
-res.end()
+const servidor = http.createServer((req, res) => {
+    res.writeHead(200, { 'Content-Type': 'text/html' })
+    if (req.url == '/') {
+        res.write('<h1>Seja bem Vindo! </h1')
+    }
+    else if (req.url == '/server') {
+        res.write('<h1>Server </h1')
+    }
+    else if (req.url == '/server/node') {
+        res.write('<h1>Server Web no Node </h1')
+    }
+    else {
+        res.write('<h1>PADRAO </h1')
+    }
+    res.end()
 })
 
-servidor.listen(porta, ()=>{console.log('Servidor Rodando')})
+servidor.listen(porta, () => { console.log('Servidor Rodando') })
--- FIC-02/Módulo02/Semana02/index.js
+++ FIC-02/Módulo02/Semana02/index.js
@@ -16,18 +16,18 @@ const porta = 443
 // Exporta o diretorio com suas subpastas e arquivos
 app.use(express.static('arquivos'));
 
-app.get('/', function(req,res){
-res.sendFile(__dirname + '/home.html')
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/home.html')
 })
-app.get('/p1', function(req,res){
-res.sendFile(__dirname + '/p1.html')
+app.get('/p1', function (req, res) {
+    res.sendFile(__dirname + '/p1.html')
 })
-app.get('/p2', function(req,res){
-res.sendFile(__dirname + '/p2.html')
+app.get('/p2', function (req, res) {
+    res.sendFile(__dirname + '/p2.html')
 })
-app.get('/p3', function(req,res){
-res.sendFile(__dirname + '/p3.html')
+app.get('/p3', function (req, res) {
+    res.sendFile(__dirname + '/p3.html')
 })
 
 
-app.listen(porta, ()=> {console.log('Servidor rodando')})
+app.listen(porta, () => { console.log('Servidor rodando') })

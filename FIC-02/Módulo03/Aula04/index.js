2ae2460cb678a677ca5505234fc7961ecd1eb2f7
dionnesbjb <dionnesbjb@hotmail.com>
2023-04-26 16:32:29 -0300

    reprogrma-se

--- FIC-02/Módulo03/Aula04/index.js
+++ FIC-02/Módulo03/Aula04/index.js
@@ -1,4 +1,5 @@
 // Importando as biliotecas
+const sqlite3 = require('sqlite3').verbose();
 const { Sequelize, Model, DataTypes } = require("sequelize");
 //Abrindo conexão com o Banco de dados ou criando um novo caso não exista
 const sequelize = new Sequelize({

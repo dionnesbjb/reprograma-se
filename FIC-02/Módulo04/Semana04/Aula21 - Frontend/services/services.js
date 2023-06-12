8cc6698e018c0bbe3f90bfd6e3012d8e0b287bea
dionnesbjb <dionnesbjb@hotmail.com>
2023-06-12 16:46:58 -0300

    reprograma-se

---
+++ FIC-02/Módulo04/Semana04/Aula21 - Frontend/services/services.js
@@ -0,0 +1,33 @@
+const axios = require("axios");
+
+module.exports = class Services {
+    //VERIFICAR USUÁRIO
+    static async UsuarioLogin(req, res) {
+        let valores = req.body;
+        const options = {
+            url: 'https://fic2-m4-semana04-aula21-backend.schimith1.repl.co/login',
+            method: 'POST',
+            data: valores
+        };
+
+        axios(options).then((usuario) => {
+            if (usuario != undefined) {
+                return res.render('logado');
+            }
+        })
+    }
+    //Create usuário
+    static async UsuarioCreate(req, res) {
+        let valores = req.body;
+        const options = {
+            url: 'https://fic2-m4-semana04-aula21-backend.schimith1.repl.co/add_usuario',
+            method: 'POST',
+            data: valores
+        };
+
+        axios(options);
+        const mensagem = "Cadastro realizado com sucesso!";
+        res.render("mensagem", { mensagem });
+    }
+
+}

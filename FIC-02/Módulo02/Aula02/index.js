79a9eb7773c4b53ff82cda4ab7226269ed2575d3
dionnesbjb <dionnesbjb@hotmail.com>
2023-07-17 10:44:33 -0300

    Reprograme-se

--- FIC-02/Módulo02/Aula02/index.js
+++
@@ -1,9 +0,0 @@
-const http = require('http');
-const porta = 443;
-
-const servidor = http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' })
-  res.write('Primeiro servidor Node')
-  res.end()
-});
-servidor.listen(porta, () => {console.log('Servidor rodando') });
--- FIC-02/Módulo02/Aula06/index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const app = express()
-const porta = 443
-
-app.get('/', (req, res) => {
-res.send('Bem vindo ao Servidor Web utilizando Express!')
-})
-
-app.listen(porta, () => { console.log('Servidor rodando') })

4e531f044d2b3945f1aee0c37dc2b84eced436ac
dionnesbjb <dionnesbjb@hotmail.com>
2023-10-09 17:29:05 -0300

    Reprograme-se

---
+++ 2023/FIC-01/Módulo03/Semana04/index.js
@@ -0,0 +1,21 @@
+const prompt = require('prompt-sync')();
+
+alturas = []
+
+for (var i = 0; i < 10; i++) {                        // 2 pontos
+  a = parseFloat(console.log("Digite a altura: "));
+  alturas.push(a);                                    // 2 pontos
+}
+
+for (var i = 0; i < 10; i++) {                        // 2 pontos
+  var cont = 0;
+
+  for (var j = 0; j < 10; j++) {                      // 2 pontos
+    if (alturas[i] > alturas[j]) {                    // 3 pontos
+      cont = cont + 1;                                // 2 pontos
+    }
+  }
+
+  // saída de dados (2 pontos)
+  console.log("Aluno " + i + ": maior que " + cont + " aluno(s)");
+}

2ed51a3b5a254fff77cdfb109613b3d73aaea40f
dionnesbjb <dionnesbjb@hotmail.com>
2023-12-18 18:38:51 -0300

    reprograme-se

---
+++ 2023/FIC-02/Modulo01/Semana02/pratica1.js
@@ -0,0 +1,42 @@
+const prompt = require('prompt-sync')();
+
+dayName = new Array ("domingo", "segunda", "terça", "quarta", "quinta", "sexta", "sábado");
+monName = new Array ("janeiro", "fevereiro", "março", "abril", "maio", "junho", "agosto", "outubro", "novembro", "dezembro")
+now = new Date
+console.log("Hoje é " + dayName[now.getDay()] + ", " + now.getDate () + " de " + monName[now.getMonth()] + " de " + now.getFullYear () );
+
+var placa = prompt('Entre com a placa do carro: ');
+
+var weekday = new Date().getDay();
+
+if(placa.endsWith('1') || placa.endsWith('2')){
+  if(weekday != 1){
+    console.log('Veículo liberado para circular');
+  }else{
+    console.log('Veículo proibido de circular');
+  }
+}else if(placa.endsWith('3') || placa.endsWith('4')){
+  if(weekday != 2){
+    console.log('Veículo liberado para circular');
+  }else{
+    console.log('Veículo proibido de circular');
+  }
+}else if(placa.endsWith('5') || placa.endsWith('6')){
+  if(weekday != 3){
+    console.log('Veículo liberado para circular');
+  }else{
+    console.log('Veículo proibido de circular');
+  }
+}else if(placa.endsWith('7') || placa.endsWith('8')){
+  if(weekday != 4){
+    console.log('Veículo liberado para circular');
+  }else{
+    console.log('Veículo proibido de circular');
+  }
+}else{
+  if(weekday != 5){
+    console.log('Veículo liberado para circular');
+  }else{
+    console.log('Veículo proibido de circular');
+  }
+}
